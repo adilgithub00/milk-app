8d23048eaf564ccf37d58864cab08c0569a9aa13
Adil <adil@seromatic.pk>
2025-12-19 04:43:03 +0500

    Update milk portal features

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\CalculatorController;
 use App\Http\Controllers\MilkEntryController;
 use App\Http\Controllers\YearlyReportController;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MilkEntryController::class, 'index']);
@@ -14,6 +15,15 @@ Route::get('/calculator', [CalculatorController::class, 'index']);
 Route::post('/payment', [CalculatorController::class, 'storePayment'])->name('payment.store');
 
 Route::get('/yearly-report', [YearlyReportController::class, 'index']);
+
+Route::get('/drop-sessions', function () {
+    try {
+        DB::statement('DROP TABLE IF EXISTS sessions');
+        return "Sessions table dropped successfully.";
+    } catch (\Exception $e) {
+        return $e->getMessage();
+    }
+});
 
 Route::get('/migrate-now', function () {
     try {
