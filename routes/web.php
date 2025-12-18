a9cc9529243e65e123e9e06ff15a4caff3979ea3
Adil <adil@seromatic.pk>
2025-12-19 03:41:09 +0500

    Update milk portal features

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,8 @@
 use App\Http\Controllers\CalculatorController;
 use App\Http\Controllers\MilkEntryController;
 use App\Http\Controllers\YearlyReportController;
-use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MilkEntryController::class, 'index']);
 Route::post('/milk-entry', [MilkEntryController::class, 'store'])
@@ -15,7 +15,7 @@ Route::post('/payment', [CalculatorController::class, 'storePayment'])->name('pa
 
 Route::get('/yearly-report', [YearlyReportController::class, 'index']);
 
-Route::get('/migrate-now', function() {
-    Artisan::call('migrate', ["--force"=> true]);
-    return "Migrations run successfully";
-});
+Route::get('/migrate-now', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    return 'Migrations run successfully';
+})->withoutMiddleware('csrf');
